7ddac66c84d78ed968adf40577d202925e7c7451
Dylanhzl <hengzldylan@gmail.com>
2026-03-23 00:14:13 +0800

    merge main to dylan

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "A user must have username"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required : [true, "A user must have password"]
+    },
+    email: {
+        type: String,
+        required: [true, "A user must have email"],
+        unique: true
+    }
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema, "users");
+
+// functions
+
+//get all of the user in database
+exports.retrieveAll = function() {
+    return User.find();
+};
+
+exports.findOneUsername = function(username) {
+    return User.findOne({username: username});
+};
+
+exports.createUser = function(userData) {
+    return User.create(userData);
+};
+
+exports.findById = function(id){
+    return User.findById(id);
+};
+
+exports.findByIdAndUpdate = function(id, updateData, options) {
+    return User.findByIdAndUpdate(id, updateData, options);
+};
+
+exports.findOneEmail = function(email) {
+    return User.findOne({email: email});
+};
+
+exports.findByIdAndDelete = function(id) {
+    return User.findByIdAndDelete(id);
+};
