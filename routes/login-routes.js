d5c7c24e13bba08adc4ddbcd50fca29ad032f5f2
l21000112 <mizukoru17@gmail.com>
2026-03-23 12:28:08 +0800

    fixed

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -3,11 +3,6 @@ const loginController = require(".././controllers/login-controller");
 
 const router = express.Router();
 
-const router = express.Router();
-
-router.get("/login", loginController.someFunction);
-
-router.post("/login-attempt", loginController.someFunction);
 router.get("/login", loginController.showLoginPage);
 router.post("/login-attempt", loginController.loginAttempt);
 router.get("/logout", loginController.logout);
