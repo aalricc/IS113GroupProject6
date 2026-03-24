080e6c8ab9aa7c897628c92eafeddda1d69c08b4
aalriccc <angchengzuo@gmail.com>
2026-03-24 13:38:37 +0800

    Update watchlist-routes.js

--- routes/watchlist-routes.js
+++ routes/watchlist-routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const authMiddleware = require('.././middleware/auth-middleware');
 
 const watchlistController = require("./../controllers/watchlist-controller");
 
 const router = express.Router();
 
-router.get("/", watchlistController.showWatchlist);
+router.get("/", authMiddleware, watchlistController.showWatchlist);
 
-router.post("/removeMovie", watchlistController.removeMovie);
-router.post("/markWatched", watchlistController.markWatched);
-router.post("/markUnwatched", watchlistController.markUnwatched);
-router.post("/createMovie", watchlistController.createWatchlist);
+router.post("/removeMovie", authMiddleware, watchlistController.removeMovie);
+router.post("/markWatched", authMiddleware, watchlistController.markWatched);
+router.post("/markUnwatched", authMiddleware, watchlistController.markUnwatched);
+router.post("/createMovie", authMiddleware, watchlistController.createWatchlist);
 module.exports = router;
