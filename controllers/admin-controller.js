2af1ba7bbd7fe4c1d59875d089b0f5fc669f84ee
kathleenlim-2025 <kathleenlim.2025@computing.smu.edu.sg>
2026-03-24 17:27:26 +0800

    admin stuff

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,3 +1,59 @@
-exports.showAdminPage = (req, res) => {
-    res.render("admin");
-}
+const { Review } = require("../models/moviereviews-model");
+const User = require("../models/user")
+
+
+exports.showAdminPage = async (req, res) => {
+    try {
+        const reviews = await Review.find();
+        const users = await User.retrieveAll();
+
+        res.render("admin", { reviews, users });
+    } catch (error) {
+        console.error("Error occured fetching admin data", error);
+        res.status(500).send("Error loading admin page");
+    }
+};
+
+exports.deleteReviewAsAdmin = async (req, res) => {
+    try {
+        await Review.findByIdAndDelete(req.params.reviewId);
+        res.redirect("/admin-page");
+    } catch (error) {
+        console.error("Error deleting review", error);
+        res.status(500).send("Could not delete review");
+    }
+};
+
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await User.findByIdAndDelete(id);
+        res.redirect("/admin-page");
+    } catch (error) {
+        console.error("Error deleting user", error);
+        res.status(500).send("Could not delete user");
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        console.log("edit route hit");
+        console.log(req.params.id);
+        console.log(req.body);
+
+        const id = req.params.id;
+        const { username, email } = req.body;
+
+        await User.findByIdAndUpdate(
+            id,
+            { username, email },
+            { runValidators: true }
+        );
+
+        res.redirect("/admin-page");
+    } catch (error) {
+        console.error("Error updating user", error);
+        res.status(500).send("Could not update user");
+    }
+};
