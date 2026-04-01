85cbacf2c756c8bbb9bae4bf23fedf02d3f795d6
l21000112 <mizukoru17@gmail.com>
2026-04-01 15:12:47 +0800

    Update moviereviews-routes.js

--- routes/moviereviews-routes.js
+++ routes/moviereviews-routes.js
@@ -5,17 +5,17 @@ const moviereviewsController = require("./../controllers/moviereviews-controller
 
 const router = express.Router();
 
-const {checkIfLogged} = require("../middleware/auth-middleware.js")
+const { checkIfLogged, checkIfAdmin } = require("../middleware/auth-middleware.js");
+
+router.post("/movie-trailer/:movieId", checkIfLogged, checkIfAdmin, moviereviewsController.createTrailer);
+
+router.post("/update-trailer/:movieId", checkIfLogged, checkIfAdmin, moviereviewsController.updateTrailer);
+
+router.post("/delete-trailer/:movieId", checkIfLogged, checkIfAdmin, moviereviewsController.deleteTrailer);
 
 router.get('/movie-reviews/:id', moviereviewsController.moviereviews);
 
 router.post("/movie-reviews/:id", checkIfLogged,moviereviewsController.postReview);
-
-router.post("/movie-trailer/:movieId", checkIfLogged, moviereviewsController.createTrailer)
-
-router.post("/update-trailer/:movieId", checkIfLogged, moviereviewsController.updateTrailer)
-
-router.post("/delete-trailer/:movieId", checkIfLogged, moviereviewsController.deleteTrailer)
 
 router.post("/delete-review/:reviewId/:movieId", checkIfLogged,moviereviewsController.deleteReview)
 
